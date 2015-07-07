271b06a582651b9c7fbab582c2a0cd97d9e6812f
Bogdan Chadkin <trysound@yandex.ru>
2015-07-08 01:10:51 +0300

    Moved variables to module

---
+++ test/variable.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var replaceVariable = require('../lib/variable');
+var should = require('should');
+var fs = require('fs');
+
+describe('## variable', function() {
+  var result = fs.readFileSync('test/fixtures-variable/result.html', 'utf-8');
+
+  it('# basic', function(done) {
+    var index = fs.readFileSync('test/fixtures-variable/index.html', 'utf-8');
+
+    replaceVariable(index, {
+      param1: 'value1',
+      obj: {
+        param1: 'o1',
+        param2: 'o2'
+      },
+      param2: 'value2'
+    }, {
+      prefix: '//='
+    }).should.equal(result);
+
+    done();
+  });
+
+  it('# with suffix', function(done) {
+    var index = fs.readFileSync('test/fixtures-variable/index-suffix.html', 'utf-8');
+
+    replaceVariable(index, {
+      param1: 'value1',
+      obj: {
+        param1: 'o1',
+        param2: 'o2'
+      },
+      param2: 'value2'
+    }, {
+      prefix: '//=',
+      suffix: '@@'
+    }).should.equal(result);
+
+    done();
+  });
+});
