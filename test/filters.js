5a5f9ca55e66ac8b68014665e3d91928f570c79e
Brandon W <walshb1@hawkmail.newpaltz.edu>
2017-12-03 01:40:50 -0500

    Update applyFilters method to look for filter handler options (#146)
    * update applyFilters method's inspect to match for options
    
    'The applyFilters method will now check the match for a json object
    and, if found, pass it as a secont paramenter in your filter hander.
    Useful for stating how you want your filter to address a given include
    in the context of the @@include statement itself.' -@andremalkine
    
    * only pass filter options if a filter handler is present
    
    * write test case for handler options
    
    * remove yarn lock

--- test/filters.js
+++ test/filters.js
@@ -107,6 +107,43 @@ describe('## gulp-file-include', () => {
       stream.write(file);
       stream.end();
     });
+
+    it('file - filters: custom filter handler options', done => {
+      var file = new gutil.File({
+        path: 'test/fixtures/index-handler-options.html',
+        contents: fs.createReadStream('test/fixtures/index-handler-options.html')
+      });
+
+      var stream = fileIncludePlugin({
+        prefix: '@@',
+        basepath: '@file',
+        filters: {
+          handler: function(content, options) {
+            should.exist(options);
+            should.exist(options.age);
+            should.exist(options.name);
+
+            // other handler code or
+            // template engine compiling here, i.e.:
+            // var template = Handlebars.compile(content);
+            // return template(options);
+
+            return content;
+          }
+        }
+      });
+
+      stream.on('data', newFile => {
+        should.exist(newFile);
+        should.exist(newFile.contents);
+
+        String(newFile.contents).should.equal(result);
+        done();
+      });
+
+      stream.write(file);
+      stream.end();
+    });
   });
 });
 
